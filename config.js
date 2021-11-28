20d9436ac48885e7c18d9000130cd1f635ca07a0
Trguo <95178004+Trguo@users.noreply.github.com>
2021-11-28 22:31:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy và câu hỏi tớ mún hỏi là cậu coá iu tớ hogg?🤗',
+    btnYes: 'iuu lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yeu thi yeu con ko yeu thi van iuu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Mốt dii học lại, mình đón đi chơi nha.',
+    messDesc: 'Mốt dii học lại, mình đón đi un tra sua nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/wer.kwang' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
